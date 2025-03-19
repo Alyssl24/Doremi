8e512f7ca6fe1c114a27a87e34af6ff049d06f05
Alyssia <ninaly.leclerc@gmail.com>
2025-03-19 14:28:32 +0100

    Resolved #9 : Create

--- src/main/java/doremi/controllers/BandController.java
+++ src/main/java/doremi/controllers/BandController.java
@@ -2,11 +2,17 @@ package doremi.controllers;
 
 import doremi.domain.Band;
 import doremi.services.BandAlbumService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
 public class BandController {
@@ -28,6 +34,20 @@ public class BandController {
         }
         model.addAttribute("band", band);
         return "bandShow";
+    }
+
+    @GetMapping("/band/new")
+    public String showForm(Band band) {
+        return "bandForm";
+    }
+
+    @PostMapping("/band")
+    public String saveBand(@Valid Band band, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "bandForm";
+        }
+        Band savedBand = bandAlbumService.save(band);
+        return "redirect:/band/" + savedBand.getId();
     }
 
 }
